1447a8109586f9f75771c12d3c28c10fa07e0df5
Yusuke-kun <japamnk@gmail.com>
2021-01-07 12:39:58 -0300

    Project finalizado

---
+++ client/js/app-es6/services/HttpService.js
@@ -0,0 +1,23 @@
+export class HttpService {
+  _handleErrors(res) {
+    if (res.ok) {
+      return res;
+    } else {
+      throw new Error(res.statusText);
+    }
+  }
+
+  get(url) {
+    return fetch(url)
+      .then((res) => this._handleErrors(res))
+      .then((res) => res.json());
+  }
+
+  post(url, dado) {
+    return fetch(url, {
+      headers: { "Content-type": "application/json" },
+      method: "POST",
+      body: JSON.stringify(dado),
+    }).then((res) => this._handleErrors(res));
+  }
+}
